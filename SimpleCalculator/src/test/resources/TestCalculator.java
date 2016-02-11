ef61033f5af15be95128179bb793e2669486b609
KhudanJeugene <khudanjevhenii@gmail.com>
2016-02-11 11:56:43 +0200

    Change 11 February

--- SimpleCalculator/src/test/resources/TestCalculator.java
+++ SimpleCalculator/src/test/resources/TestCalculator.java
@@ -1,98 +1,94 @@
-import static org.junit.Assert.fail;
-
+import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-
-import org.junit.After;
-import org.junit.Before;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
-public class TestCalculatorIDE {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+public class Aaa1 
+{
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-  @Before
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "file:///E:/Workspace2/SimpleCalculator/src/main/resources/Calculator.html";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	@Before
+	public void setUp() throws Exception 
+	{
+		driver = new FirefoxDriver();
+		baseUrl = "file:///E:/Workspace2/SimpleCalculator/src/main/resources/Calculator.html";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-  @Test
-  public void testCalculatorIDE() throws Exception {
-    driver.get(baseUrl + "file:///E:/Workspace2/SimpleCalculator/src/main/resources/Calculator.html");
-    driver.findElement(By.id("1")).click();
-    driver.findElement(By.id("2")).click();
-    driver.findElement(By.id("3")).click();
-    driver.findElement(By.id("+")).click();
-    driver.findElement(By.id("5")).click();
-    driver.findElement(By.id("6")).click();
-    driver.findElement(By.id("3")).click();
-    driver.findElement(By.id("=")).click();
-    driver.findElement(By.id("5")).click();
-    driver.findElement(By.id("6")).click();
-    driver.findElement(By.id("3")).click();
-    driver.findElement(By.id("/")).click();
-    driver.findElement(By.id("4")).click();
-    driver.findElement(By.id("5")).click();
-    driver.findElement(By.id("=")).click();
-    driver.findElement(By.id("clear")).click();
-    driver.findElement(By.id("4")).click();
-    driver.findElement(By.id("5")).click();
-    driver.findElement(By.id("2")).click();
-    driver.findElement(By.id("+")).click();
-    driver.findElement(By.id("6")).click();
-    driver.findElement(By.id("5")).click();
-    driver.findElement(By.id("3")).click();
-    driver.findElement(By.id("=")).click();
-  }
+	@Test
+	public void testAaa1() throws Exception 
+	{
+		driver.get(baseUrl + "file:///E:/Workspace2/SimpleCalculator/src/main/resources/Calculator.html");
+		driver.findElement(By.id("1")).click();
+		assertEquals("res", driver.findElement(By.tagName("BODY")).getText());
+		driver.findElement(By.id("2")).click();
+		assertEquals("res", driver.findElement(By.tagName("BODY")).getText());
+		driver.findElement(By.id("+")).click();
+		assertEquals("id=res", driver.findElement(By.tagName("BODY")).getText());
+		driver.findElement(By.id("1")).click();
+		driver.findElement(By.id("2")).click();
+		assertEquals("res", driver.findElement(By.tagName("BODY")).getText());
+		driver.findElement(By.id("=")).click();
+		assertEquals("res", driver.findElement(By.tagName("BODY")).getText());
+	}
 
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
+	@After
+	public void tearDown() throws Exception 
+	{
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+	private boolean isElementPresent(By by) 
+	{
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) 
+		{
+			return false;
+		}
+	}
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+	private boolean isAlertPresent() 
+	{
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) 
+		{
+			return false;
+		}
+	}
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+	private String closeAlertAndGetItsText() 
+	{
+		try 
+		{
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} 
+			else 
+			{
+				alert.dismiss();
+			}
+			return alertText;
+		} 
+		finally 
+		{
+			acceptNextAlert = true;
+		}
+	}
 }
-
---
+++ SimpleCalculator/src/test/resources/TestCalculatorTest.groovy
@@ -0,0 +1,12 @@
+/**
+ * Created by Женя on 11.02.2016.
+ */
+class TestCalculatorTest extends groovy.util.GroovyTestCase {
+    void testTestCall_1() {
+
+    }
+
+    void testTearDown() {
+
+    }
+}
